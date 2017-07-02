7854db6ecb7291b6aa23890eeb28f5660b749fb1
johnmcdnl <john.mcdonnell439@gmail.com>
2017-07-02 13:53:20 +0100

    adding a piece list@

---
+++ chess/pieces.go
@@ -0,0 +1,26 @@
+package chess
+
+const (
+	_ = iota
+	PieceTypePawn
+	PieceTypeKnight
+	PieceTypeBishop
+	PieceTypeRook
+	PieceTypeQueen
+	PieceTypeKing
+)
+const (
+	_           = iota
+	WhitePawn   = PieceTypePawn
+	WhiteKnight = PieceTypeKnight
+	WhiteBishop = PieceTypeBishop
+	WhiteRook   = PieceTypeRook
+	WhiteQueen  = PieceTypeQueen
+	WhiteKing   = PieceTypeKing
+	BlackPawn   = PieceTypePawn
+	BlackKnight = PieceTypeKnight
+	BlackBishop = PieceTypeBishop
+	BlackRook   = PieceTypeRook
+	BlackQueen  = PieceTypeQueen
+	BlackKing   = PieceTypeKing
+)
