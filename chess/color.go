d48230e27004e363c97add87593b4747c5cb8618
johnmcdnl <john.mcdonnell439@gmail.com>
2017-07-09 16:17:02 +0100

    define colors

---
+++ chess/color.go
@@ -0,0 +1,11 @@
+package chess
+
+const (
+	colorWhite = iota
+	colorBlack
+)
+
+type Color int
+
+const White = Color(colorWhite)
+const Black = Color(colorBlack)
---
+++ chess/color_test.go
@@ -0,0 +1,12 @@
+package chess
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestColors(t *testing.T) {
+	assert.Equal(t, White, Color(0))
+	assert.Equal(t, Black, Color(1))
+	assert.NotEqual(t, White, Black)
+}
