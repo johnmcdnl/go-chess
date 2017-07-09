6135e29a868c9a7bbd4ced59ac930f50f4d3acb4
johnmcdnl <john.mcdonnell439@gmail.com>
2017-07-09 20:19:46 +0100

    basic pawn movement

--- chess/pawn.go
+++ chess/pawn.go
@@ -12,10 +12,61 @@ func NewPawn(s *Square, c Color) *Pawn {
 	return &b
 }
 
-func (p *Pawn) ValidMoves(board *Board) []*Move {
-	var arr []*Move
-	return arr
+func (p *Pawn) ValidMoves(b *Board) []*Move {
+	switch p.Color{
+	default:
+		return []*Move{}
+	case White:
+		return p.whiteMoves(b)
+	case Black:
+		return p.blackMoves(b)
+	}
 }
+
+func (p *Pawn)whiteMoves(b *Board) []*Move {
+	var moves []*Move
+
+	pos := p.Position
+	currentFile := pos.File
+	currentRank := pos.Rank
+
+	if currentRank + 1 < 8 {
+		if m, err := NewMove(pos, b.GetSquare(currentFile, currentRank + 1)); err == nil {
+			moves = append(moves, m)
+		}
+	}
+
+	if currentRank == 2 {
+		if m, err := NewMove(pos, b.GetSquare(currentFile, currentRank + 2)); err == nil {
+			moves = append(moves, m)
+		}
+	}
+
+	return moves
+}
+
+func (p *Pawn)blackMoves(b *Board) []*Move {
+	var moves []*Move
+
+	pos := p.Position
+	currentFile := pos.File
+	currentRank := pos.Rank
+
+	if currentRank - 1 > 0 {
+		if m, err := NewMove(pos, b.GetSquare(currentFile, currentRank - 1)); err == nil {
+			moves = append(moves, m)
+		}
+	}
+
+	if currentRank == 7 {
+		if m, err := NewMove(pos, b.GetSquare(currentFile, currentRank - 2)); err == nil {
+			moves = append(moves, m)
+		}
+	}
+
+	return moves
+}
+
 func (p *Pawn) GetCode() int {
 	return p.Code
 }
