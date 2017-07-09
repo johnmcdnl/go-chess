857859db3a7a2f13d95c5a2bd6fe614c263d6f29
johnmcdnl <john.mcdonnell439@gmail.com>
2017-07-09 16:12:31 +0100

    reset

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
+	"github.com/johnmcdnl/go-chess/engine"
 	"fmt"
-
-	"github.com/johnmcdnl/go-chess/chess"
 )
 
 func main() {
-	fmt.Println("Welcome to go-chess")
-	b := chess.NewBoard()
-	chess.GlobalBoard = b
 
-	for _, square := range b.Squares {
-		if square.CurrentPiece != nil && square.CurrentPiece.GetPiece().Type==chess.PieceTypePawn {
-			for _, m := range square.CurrentPiece.ValidMoves().BestMoves{
-				fmt.Println(m.Name())
-			}
-		}
-	}
 }
