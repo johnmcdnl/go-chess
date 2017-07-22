dc341f498e9a4c5f3e70bd8b2b3c0e90f5cb96f8
John McDonnell <john.mcdonnell439@gmail.com>
2017-07-22 16:28:07 +0100

    basic fix to gucumber

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func main() {
 	fmt.Println()
 	fmt.Println()
 	for _, s := range b.Squares {
-		if s.CurrentPiece != nil && s.CurrentPiece.PieceType()==chess.RookType{
+		if s.CurrentPiece != nil && s.CurrentPiece.PieceType() == chess.RookType {
 			fmt.Println()
-			for _, m:=range s.CurrentPiece.ValidMoves(b){
+			for _, m := range s.CurrentPiece.ValidMoves(b) {
 				fmt.Println(m.Printer())
 			}
 			fmt.Println()
