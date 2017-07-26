aaf9a3221f2d20cdb33f0093fad6851d59037662
johnmcdnl <john.mcdonnell439@gmail.com>
2017-07-26 20:02:53 +0100

    extend benchmarks for kights

--- chess/knight_bench_test.go
+++ chess/knight_bench_test.go
@@ -4,9 +4,27 @@ import (
 	"testing"
 )
 
-func BenchmarkKnight_ValidMoves(b *testing.B) {
+func BenchmarkKnight_ValidMovesNearCenter(b *testing.B) {
 	board, _ := BoardFEN("4k3/8/1q6/3N4/8/8/8/4K3 w - - 0 1")
 	knight := board.GetSquare(D, 5).CurrentPiece
+
+	for i := 0; i < b.N; i++ {
+		knight.ValidMoves(board)
+	}
+}
+
+func BenchmarkKnight_ValidMovesFirstColumn(b *testing.B) {
+	board, _ := BoardFEN("4k3/8/8/8/N7/8/8/4K3 w - - 0 1")
+	knight := board.GetSquare(A, 4).CurrentPiece
+
+	for i := 0; i < b.N; i++ {
+		knight.ValidMoves(board)
+	}
+}
+
+func BenchmarkKnight_ValidMovesSecondColumn(b *testing.B) {
+	board, _ := BoardFEN("4k3/8/8/8/1N6/8/8/4K3 w - - 0 1")
+	knight := board.GetSquare(B, 4).CurrentPiece
 
 	for i := 0; i < b.N; i++ {
 		knight.ValidMoves(board)
